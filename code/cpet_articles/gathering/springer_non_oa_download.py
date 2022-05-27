8292d681ad8245f58ae7d13051a39c02e7813c82
Anton Hesse <ahesse2567@gmail.com>
2022-05-27 09:23:25 -0500

    Downloading more pdfs and moving csvs

--- code/cpet_articles/gathering/springer_non_oa_download.py
+++ code/cpet_articles/gathering/springer_non_oa_download.py
@@ -5,98 +5,65 @@ import requests
 # from unpywall import Unpywall
 import json
 import re
+from tqdm import tqdm
 
 # UnpywallCredentials('hesse151@umn.edu')
 
-articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/ovid/unpaywall_info.csv')
+articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv')
 springer_non_oa_articles = articles[(articles['is_oa'] == False) & \
     (articles['publisher'] == 'Springer Science and Business Media LLC')].reset_index()
 
 with open('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/code/cpet_articles/gathering/springer_config.json') as config_file:
     api_key = json.load(config_file)['api_key']
 api_key
-# api_key = '94ac47d7af9676b7403b4694dcf058b0'
-
-query = springer_non_oa_articles.loc[0,'doi']
-query
-params = {
-    'api_key': api_key,
-    'q': query
-    }
-params
 
 springer_api_url = 'https://api.springernature.com/meta/v2/json?'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0'}
+folder = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/pdfs/springer_non_oa_pdfs/'
 
-response = requests.get(url = springer_api_url, params = params, headers=headers, allow_redirects=True)
-response
-response.json()
-response.json()['records'][0]['url']
-pdf_url = response.json()['records'][0]['url'][1]['value']
-pdf_url
+query_status_code = []
+pdf_status_code = []
+pbar = tqdm(len(springer_non_oa_articles.index))
 
-pdf_response = requests.get(pdf_url)
-pdf_response
-pdf_response.content
+for i, row in springer_non_oa_articles.iterrows():
+    query = row['doi']
+    params = {
+    'api_key': api_key,
+    'q': query
+    }
 
-with open(filename, mode = 'wb') as f:
-    f.write(pdf_response.content)
+    try:
+        response = requests.get(url = springer_api_url, params = params, headers=headers, allow_redirects=True)
+        if response.status_code == 200:
+            query_status_code.append(response.status_code)
 
-id_param = re.compile(r'openurl/pdf\?id=doi\:')
-new_pdf_url = id_param.sub('content/pdf/', pdf_url) + '.pdf'
-fix_https = re.compile(r'http')
-new_pdf_url = fix_https.sub('https', new_pdf_url)
-new_pdf_url
+            pdf_url = response.json()['records'][0]['url'][1]['value']
+            pdf_response = requests.get(pdf_url, stream=True, allow_redirects=True)
+            if pdf_response.status_code == 200:
+                pdf_status_code.append(pdf_response.status_code)
 
-# https://link.springer.com/content/pdf/10.1007/s00421-021-04808-z.pdf
-# https://link.springer.com/content/pdf/10.1007/s00421-021-04808-z.pdf
+                # drop_prefix = str(row['doi'].split('/')[1:]).strip("[']")
+                # filename = folder + str(drop_prefix)+'.pdf'
 
-pdf_response = requests.get(new_pdf_url)
-pdf_response
-pdf_response.content
+                # with open(filename, mode = 'wb') as f:
+                #     f.write(pdf_response.content)
+            
+            else:
+                print('Bad pdf URL response:', response.status_code, 'for', pdf_response.url)
+                pdf_status_code.append(pdf_response.status_code)
+        
+        else:
+            print('Bad query URL response:', response.status_code, 'for', response.url)
+            query_status_code.append(response.status_code)
 
-drop_prefix = str(springer_non_oa_articles.loc[0,'doi'].split('/')[1:]).strip('[').strip("']")
-folder = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/ovid/springer_non_oa_pdfs/'
-filename = folder + str(drop_prefix)+'.pdf'
-filename
+    except Exception as e:
+        print(f'{e} at index {i} for url {query}')
+        query_status_code.append(e)
+        # pdf_status_code.append(e)
+        
+    pbar.update(1)
 
-with open(filename, mode = 'wb') as f:
-    f.write(pdf_response.content)
+pbar.close()
 
-
-
-################
-
-test_url = 'https://link.springer.com/content/pdf/10.1007/s00421-021-04808-z.pdf'
-
-
-pdf_response = requests.get(test_url, headers=headers, allow_redirects=True)
-pdf_response
-pdf_response.content
-
-drop_prefix = str(springer_non_oa_articles.loc[0,'doi'].split('/')[1:]).strip('[').strip("']")
-folder = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/ovid/springer_non_oa_pdfs/'
-filename = folder + str(drop_prefix)+'.pdf'
-filename
-
-with open(filename, mode = 'wb') as f:
-    f.write(pdf_response.content)
-
-
-
-# id_param = re.compile(r'pdf\?id=doi\:')
-# new_pdf_url = id_param.sub('pdf/', pdf_url)
-# new_pdf_url
-
-# new_pdf_response = requests.get(new_pdf_url)
-
-drop_prefix = str(springer_non_oa_articles.loc[0,'doi'].split('/')[1:]).strip('[').strip("']")
-folder = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/ovid/springer_non_oa_pdfs/'
-filename = folder + str(drop_prefix)+'.pdf'
-filename
-
-with open(filename, mode = 'wb') as f:
-    f.write(pdf_response.content)
-
-# https://api.springernature.com/meta/v2/?api_key=94ac47d7af9676b7403b4694dcf058b0&q=10.1007%2Fs00421-021-04808-z
-# https://api.springernature.com/meta/v2/json?api_key=94ac47d7af9676b7403b4694dcf058b0&q=10.1007%2Fs00421-021-04808-z&s=1&p=10
+# I need to fix an error where some of the status code lists get double counted
+# or I need to make the status codes part of a data frame with the doi
--- code/cpet_articles/gathering/ssl_error_downloads.py
+++ code/cpet_articles/gathering/ssl_error_downloads.py
@@ -3,27 +3,30 @@ import requests
 import pandas as pd
 from unpywall.utils import UnpywallCredentials
 from unpywall import Unpywall
+from tqdm import tqdm
 
 UnpywallCredentials('hesse151@umn.edu')
 # %%
-ssl_error_articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_data_analysis/data/cpet_articles/ovid/ssl_error.csv')
+oa_articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/oa_articles_status_codes.csv')
+ssl_oa_articles = oa_articles[oa_articles['status_code'].str.contains('SSL')]
+
 # %%
+ssl_status_code = []
 
-for idx, row in ssl_error_articles.iterrows():
-
+for idx, row in tqdm(ssl_oa_articles.iterrows()):
     url = Unpywall.get_pdf_link(doi = row.doi_url)
     
     if url:
         # this just checks to make sure there's a URL before sending the request
         try:
             response = requests.get(url, verify=False)
-
-            if response.status_code == 200:
-                # save the PDF if the server responds
+            # verify=False required to fix ssl errors
+            if response.status_code == 200: # save the PDF if the server responds
+                ssl_status_code.append(response.status_code)
 
                 # drop the doi prefix so you can use the DOI as a filename
                 drop_prefix = row.doi.split('/')[1]
-                folder_name = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_data_analysis/data/cpet_articles/ovid/ovid_pdfs/'
+                folder_name = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/pdfs/unpaywall_oa_ssl/'
                 filename = folder_name + str(drop_prefix)+'.pdf'
 
                 # write the PDF
@@ -31,14 +34,15 @@ for idx, row in ssl_error_articles.iterrows():
                     f.write(response.content)
 
             else:
-                # if the server response is bad, add code here to add a column to oa_articles that saves the response.status_code
-                print('Bad URL response:', response.status_code, 'for', url)
+                print('Bad URL response:', response.status_code, 'for', url, 'at idx', idx)
+                ssl_status_code.append(response.status_code)
         except Exception as e:
             print(f'{e} at index {idx} for url {url}')
+            ssl_status_code.append(e)
 
-    else:
-        # add code here to add a column with note that there no PDF URL available
+    else: # if Unpywall can't get a url
         print('No URL available from unpaywall')
+        ssl_status_code.append('No URL available from unpaywall')
 
 # %%
-ssl_error_articles.to_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_data_analysis/data/cpet_articles/ovid/oa_articles_status_codes.csv')
+ssl_oa_articles.to_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/ssl_oa_articles')
