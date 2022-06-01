a118129f0f9517004c2b5ad158ef554bcdb88f0d
Anton Hesse <ahesse2567@gmail.com>
2022-06-01 12:08:04 -0500

    Moved txt files from conversion to own folder

--- code/cpet_articles/tidying/convert_unpaywall_pdf_to_txt.py
+++ code/cpet_articles/tidying/convert_unpaywall_pdf_to_txt.py
@@ -7,12 +7,24 @@ unpaywall_files = glob.glob('data/cpet_articles/pdfs/unpaywall_oa/*.pdf')
 
 errors = {}
 
+#  got stuck at file 503
+unpaywall_files = unpaywall_files[503:]
+
 for f in tqdm(unpaywall_files):
     txt_filename = f.split('.pdf')[0]
     cmd = 'pdf2txt.py -o %s.txt %s' % (txt_filename, f)
     run = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = run.communicate()
-    # display errors if they occur 
-    if err:
-        errors.update({f: err})
-        print(err)
+    try:
+        out, err = run.communicate(timeout=60)
+        if err:
+            errors.update({f: err})
+            print(err) # display errors if they occur
+    except Exception as e:
+        run.kill()
+        out, err = run.communicate()
+        if err:
+            errors.update({f: err})
+            print(err) # display errors if they occur
+
+# next step: move files from the same folder to another folder
+# should this just be part of the loop?
