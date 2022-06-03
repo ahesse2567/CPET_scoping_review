8388471ebbcda6496c2d4210fdf6a9e08471d49f
Anton Hesse <ahesse2567@gmail.com>
2022-06-03 07:57:25 -0500

    Still no success for Crossref + BMJ or Wiley

--- code/cpet_articles/gathering/pdf_download_code/Wiley_unpywall_downloads.py
+++ code/cpet_articles/gathering/pdf_download_code/Wiley_unpywall_downloads.py
@@ -6,32 +6,37 @@ from unpywall import Unpywall
 
 UnpywallCredentials('hesse151@umn.edu')
 
-articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_data_analysis/data/cpet_articles/ovid/oa_articles_status_codes.csv')
-wiley_articles = articles[(articles['is_oa'] == True) & \
-    (articles['publisher'] == 'Wiley') & \
-        (articles['status_code'] == '503')].reset_index(drop=True)
-
+articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv')
+wiley_articles = articles[(articles['is_oa'] == False) & \
+    (articles['publisher'] == 'Wiley')].reset_index(drop=True) # & \ # 
+    #     (articles['status_code'] == '503')].reset_index(drop=True)
+wiley_articles.shape
 # find download urls
 crossref_headers = {'Accept': 'application/vnd.citationstyles.csl+json, application/vnd.crossref.unixref+xml'}
 doi_url = wiley_articles.loc[0,'doi_url']
-crossref_response = requests.get(url = doi_url, headers=crossref_headers, allow_redirects=True)
+crossref_response = requests.get(url = doi_url, headers=crossref_headers, allow_redirects=True, verify=False)
 crossref_response.status_code
-crossref_response.json().keys()
-for k, v in crossref_response.json().items():
-    print(k, ': ', v)
 
+# crossref_response.json().keys()
+# for k, v in crossref_response.json().items():
+#     print(k, ': ', v)
 
-crossref_response.content # is the wiley api somewhere in here?
-crossref_response.url
+# crossref_response.content # is the wiley api somewhere in here?
+crossref_response.json()['link']
+pdf_url = crossref_response.json()['link'][0]['URL']
+pdf_url
 
 # trying with Crossref_response.url
 token = '08932ce3-df43-4685-b8ac-fa5f692bc235'
 # url = Unpywall.get_pdf_link(doi = wiley_articles.loc[0,'first_oa_location.url_for_pdf'])
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0',
-    'Wiley-TDM-Client-Token': token
+    'Wiley-TDM-Client-Token': token,
+    'CR-TDM-Rate-Limit': '5' ,
+    'CR-TDM-Rate-Limit-Remaining': '4',
+    'CR-TDM-Rate-Limit-Reset': '1375427514'
     }
-response = requests.get(crossref_response.url, headers = headers, allow_redirects=True)
+response = requests.get(crossref_response.url, headers = headers, allow_redirects=True, verify=False)
 response # response 406
 
 # trying with dx.doi from Crossref
