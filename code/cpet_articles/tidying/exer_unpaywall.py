d4964a5116ddf476540ee3787728ec84e524bfa7
Anton Hesse <ahesse2567@gmail.com>
2022-06-29 14:53:34 -0500

    Moving pdf and txt no exer* files

--- code/cpet_articles/tidying/exer_unpaywall.py
+++ code/cpet_articles/tidying/exer_unpaywall.py
@@ -7,5 +7,11 @@ merge = pd.merge(no_exer_dois['doi'], no_exer_unpaywall_info, how='outer', indic
 
 exer_unpaywall_info = merge[merge['_merge'] == 'right_only'].reset_index(drop=True)
 exer_unpaywall_info
+
+unpaywall_exer_new = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info_new_exer.csv')
+
+df_list = [exer_unpaywall_info, unpaywall_exer_new]
+exer_unpaywall_info = pd.concat(df_list).drop_duplicates().reset_index(drop=True)
+
 exer_unpaywall_info.to_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv',
     index=False)
