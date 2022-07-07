6b595fe0be1a7fef5acaa8dbb1c095abe334e2ea
Anton Hesse <ahesse2567@gmail.com>
2022-07-07 16:49:26 -0500

    Mostly Elsevier downloads

--- code/cpet_articles/gathering/pdf_download_code/elsevier_non_oa_downloads.py
+++ code/cpet_articles/gathering/pdf_download_code/elsevier_non_oa_downloads.py
@@ -1,123 +1,55 @@
-# %%
-import requests
 import pandas as pd
 import requests
-from unpywall.utils import UnpywallCredentials
-from unpywall import Unpywall
-from elsapy.elsclient import ElsClient
-from elsapy.elsprofile import ElsAuthor, ElsAffil
-from elsapy.elsdoc import FullDoc, AbsDoc
-from elsapy.elssearch import ElsSearch
 import json
+import random
+from tqdm import tqdm
 
-UnpywallCredentials('hesse151@umn.edu')
-
-articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/ovid/unpaywall_info.csv')
+articles = pd.read_csv('data/cpet_articles/unpaywall/unpaywall_info.csv')
 elsevier_non_oa_articles = articles[(articles['is_oa'] == False) & \
-    (articles['publisher'] == 'Elsevier BV')]
-elsevier_non_oa_articles
+    (articles['publisher'] == 'Elsevier BV')].reset_index(drop=True)
+# elsevier_non_oa_articles
 
-# %%
+# Load api key
+with open("code/cpet_articles/gathering/pdf_download_code/elsevier_config.json") as config_file:
+    api_key = json.load(config_file)['apikey']
 
-"""An example program that uses the elsapy module"""
+# see instructions from getting started guide
+# https://dev.elsevier.com/tecdoc_text_mining.html
 
-## Load configuration
-with open("/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/code/cpet_articles/gathering/elsevier_config.json") as config_file:
-    config = json.load(config_file)
+# n = random.randint(0, len(elsevier_non_oa_articles))
+# test_doi = elsevier_non_oa_articles.loc[n,'doi']
+# doi_url = 'https://api.elsevier.com/content/article/doi/' + test_doi
+# doi_url
 
-## Initialize client
-client = ElsClient(config['apikey'])
-# client.inst_token = config['insttoken']
+elsevier_params = {
+    'apiKey': api_key,
+    'httpAccept': 'text/xml',
+    'view': 'FULL'}
+elsevier_headers ={
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'}
+folder = 'data/cpet_articles/full_texts/xmls/eslevier_non_oa_xmls'
 
-## Author example
-# Initialize author with uri
-my_auth = ElsAuthor(
-        uri = 'https://api.elsevier.com/content/author/author_id/7004367821')
-# Read author data, then write to disk
-if my_auth.read(client):
-    print ("my_auth.full_name: ", my_auth.full_name)
-    my_auth.write()
-else:
-    print ("Read author failed.")
+status_codes = []
+for idx, row in tqdm(elsevier_non_oa_articles.iterrows(), total=len(elsevier_non_oa_articles)):
+    doi = row['doi']    
+    doi_url = 'https://api.elsevier.com/content/article/doi/' + doi
+    temp_dict = {'doi', doi}
 
-## Affiliation example
-# Initialize affiliation with ID as string
-my_aff = ElsAffil(affil_id = '60101411')
-if my_aff.read(client):
-    print ("my_aff.name: ", my_aff.name)
-    my_aff.write()
-else:
-    print ("Read affiliation failed.")
+    try:
+        r = requests.get(url=doi_url, params=elsevier_params, headers=elsevier_headers,
+        allow_redirects=True, verify=True)
+        temp_dict.update({'elsevier_status_code': r.status_code})
 
-## Scopus (Abtract) document example
-# Initialize document with ID as integer
-scp_doc = AbsDoc(scp_id = 84872135457)
-if scp_doc.read(client):
-    print ("scp_doc.title: ", scp_doc.title)
-    scp_doc.write()   
-else:
-    print ("Read document failed.")
+        if r.status_code == 200:
+            doi_suffix = str(doi.split('/')[1:]).strip("[']")
+            filename = f'{folder}/{doi_suffix}.xml'
+            
+            with open(filename, mode='wb') as f:
+                f.write(r.content)
 
-## ScienceDirect (full-text) document example using PII
-pii_doc = FullDoc(sd_pii = 'S1674927814000082')
-if pii_doc.read(client):
-    print ("pii_doc.title: ", pii_doc.title)
-    pii_doc.write()   
-else:
-    print ("Read document failed.")
-
-pii_doc.data.keys()
-pii_doc.data['objects']
-
-## ScienceDirect (full-text) document example using DOI
-doi_doc = FullDoc(doi = '10.1016/S1525-1578(10)60571-5')
-if doi_doc.read(client):
-    print ("doi_doc.title: ", doi_doc.title)
-    doi_doc.write()   
-else:
-    print ("Read document failed.")
-
-
-## Load list of documents from the API into affilation and author objects.
-# Since a document list is retrieved for 25 entries at a time, this is
-#  a potentially lenghty operation - hence the prompt.
-print ("Load documents (Y/N)?")
-s = input('--> ')
-
-if (s == "y" or s == "Y"):
-
-    ## Read all documents for example author, then write to disk
-    if my_auth.read_docs(client):
-        print ("my_auth.doc_list has " + str(len(my_auth.doc_list)) + " items.")
-        my_auth.write_docs()
-    else:
-        print ("Read docs for author failed.")
-
-    ## Read all documents for example affiliation, then write to disk
-    if my_aff.read_docs(client):
-        print ("my_aff.doc_list has " + str(len(my_aff.doc_list)) + " items.")
-        my_aff.write_docs()
-    else:
-        print ("Read docs for affiliation failed.")
-
-## Initialize author search object and execute search
-auth_srch = ElsSearch('authlast(keuskamp)','author')
-auth_srch.execute(client)
-print ("auth_srch has", len(auth_srch.results), "results.")
-
-## Initialize affiliation search object and execute search
-aff_srch = ElsSearch('affil(amsterdam)','affiliation')
-aff_srch.execute(client)
-print ("aff_srch has", len(aff_srch.results), "results.")
-
-## Initialize doc search object using Scopus and execute search, retrieving 
-#   all results
-doc_srch = ElsSearch("AFFIL(dartmouth) AND AUTHOR-NAME(lewis) AND PUBYEAR > 2011",'scopus')
-doc_srch.execute(client, get_all = True)
-print ("doc_srch has", len(doc_srch.results), "results.")
-
-## Initialize doc search object using ScienceDirect and execute search, 
-#   retrieving all results
-doc_srch = ElsSearch("star trek vs star wars",'sciencedirect')
-doc_srch.execute(client, get_all = False)
-print ("doc_srch has", len(doc_srch.results), "results.")
+    except Exception as e:
+        print(f'Exception at DOI {doi}')
+        print(e)
+        temp_dict.update({'error': e})
+    
+    status_codes.append(temp_dict)
