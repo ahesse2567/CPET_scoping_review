dd7fa35da722db88053c3957f0e76f2c4d0f919e
Anton Hesse <ahesse2567@gmail.com>
2022-07-14 16:47:02 -0500

    Downloaded a few more Wiley OA articles
    Still getting mostly 5xx errors

--- code/cpet_articles/gathering/full-text_download_code/wiley_oa_download.py
+++ code/cpet_articles/gathering/full-text_download_code/wiley_oa_download.py
@@ -1,11 +1,12 @@
 import sys
+import requests
 sys.path.append('code/cpet_articles/gathering/pdf_download_code')
 from crossref_pdf_download import crossref_pdf_download
 import pandas as pd
 from tqdm import tqdm
 import json
 
-with open('code/cpet_articles/gathering/pdf_download_code/wiley_config.json') as config_file:
+with open('code/cpet_articles/gathering/full-text_download_code/wiley_config.json') as config_file:
     wiley_token = json.load(config_file)['api_key']
 
 articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv')
@@ -38,15 +39,63 @@ dest_folder = 'data/cpet_articles/pdfs/wiley_oa_pdfs'
 
 df_status_codes = pd.read_csv('data/cpet_articles/unpaywall/wiley_oa_status_codes.csv')
 non200 = df_status_codes[df_status_codes['publisher_status_code'] != 200]
-res2 = []
-for i, row in tqdm(non200.iterrows(), total=non200.shape[0]):
-    temp = crossref_pdf_download(
-        doi=row['doi'],
-        accept=accept,
-        dest=dest_folder,
-        user_agent=user_agent,
-        application='pdf',
-        TDM_header='Wiley-TDM-Client-Token',
-        TDM_token=wiley_token,
-        verify=True)
-    res2.append(temp)
+non200.shape
+# res2 = []
+# for i, row in tqdm(non200.iterrows(), total=non200.shape[0]):
+#     temp = crossref_pdf_download(
+#         doi=row['doi'],
+#         accept=accept,
+#         dest=dest_folder,
+#         user_agent=user_agent,
+#         application='pdf',
+#         TDM_header='Wiley-TDM-Client-Token',
+#         TDM_token=wiley_token,
+#         verify=True)
+    # res2.append(temp)
+
+non200
+test_url = non200['best_oa_location.url_for_pdf'].iloc[1]
+test_url
+
+publisher_headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0',
+    'Wiley-TDM-Client-Token': wiley_token
+}
+folder = 'data/cpet_articles/full_texts/pdfs/wiley_oa_pdfs'
+res = []
+for idx, row in tqdm(non200.iterrows(), total=len(non200)):
+    out = {}
+    doi = row['doi']
+    out.update({'doi': doi})
+    pdf_url_best = row['best_oa_location.url_for_pdf']
+
+    try:
+        r = requests.get(url = pdf_url_best, headers=publisher_headers, allow_redirects=True, verify=True)
+        out.update({'best_oa_location_status_code': r.status_code})
+
+        if (r.status_code != 200) & (row['best_oa_location.url_for_pdf'] != row['first_oa_location.url_for_pdf']):
+            # try another possible pdf url
+            print('Trying another PDF url')
+            pdf_url_first = row['first_oa_location.url_for_pdf']
+            r = requests.get(url = pdf_url_first, headers=publisher_headers, allow_redirects=True, verify=True)
+            out.update({'first_oa_location_status_code': r.status_code})
+
+        if r.status_code == 200:
+            doi_suffix = str(doi.split('/')[1:]).strip("[']")
+            filename = f'{folder}/{doi_suffix}.pdf'
+
+            with open(filename, 'wb') as f:
+                f.write(r.content)
+        else:
+            print(f'Status code {r.status_code} for DOI {doi}')
+
+    except Exception as e:
+        out.update({'error': e})
+        print(f'Exception at DOI {doi}')
+        print(e)
+
+    res.append(out)
+
+res
+res_df = pd.DataFrame(res)
+res_df
