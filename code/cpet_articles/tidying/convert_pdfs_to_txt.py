11c92de680341d8807f19240d95857fffb460a0d
Anton Hesse <ahesse2567@gmail.com>
2022-07-22 12:06:01 -0500

    More ovid downloading, some reorgs

--- code/cpet_articles/tidying/convert_pdfs_to_txt.py
+++ code/cpet_articles/tidying/convert_pdfs_to_txt.py
@@ -6,28 +6,37 @@ import pandas as pd
 import itertools
 import pandas as pd
 
-existing_txt_file_paths = list(Path('data/cpet_articles/full_texts/txts').rglob('*.txt'))
+"""
+Flow of this file
+1.) Find all txt stem names
+2.) Find txt files in 'bad' folders
+3.) Finall all PDF stem names
+4.) Compare PDF stems agains txt stems to find PDFs that have yet to be converted
+"""
+
+#  find txt files and stems
+existing_txt_file_paths = list(Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/txts').rglob('*.txt'))
 len(existing_txt_file_paths)
 existing_txt_files = [path.stem for path in existing_txt_file_paths]
-
-existing_pdf_file_paths = list(Path('data/cpet_articles/full_texts/pdfs').rglob('*.pdf'))
-len(existing_pdf_file_paths)
-existing_pdf_files = [path.stem for path in existing_pdf_file_paths]
-
-full_text_folders = [folder for folder in list(Path('data/cpet_articles/full_texts').iterdir()) if folder.is_dir()]
+# find txt error folders
+full_text_folders = [folder for folder in list(Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts').iterdir()) if folder.is_dir()]
 txt_error_folders = ['non-english', 'empty_txt_conv', 'pdf_to_txt_parsing_error']
 txt_error_folders_paths = [folder for folder in full_text_folders if folder.name in txt_error_folders]
-
+#  find txt error files
 txt_error_file_path_list = [path.glob('*.txt') for path in txt_error_folders_paths]
 txt_error_file_paths = list(itertools.chain.from_iterable(txt_error_file_path_list))
 txt_error_files = [path.stem for path in txt_error_file_paths]
-
+# find PDF files
+existing_pdf_file_paths = list(Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/pdfs').rglob('*.pdf'))
+len(existing_pdf_file_paths)
+existing_pdf_files = [path.stem for path in existing_pdf_file_paths]
+# determine PDFs that have yet to be converted
 txt_error_files = [path.stem for path in txt_error_file_paths]
 pdfs_to_convert = [pdf for pdf in existing_pdf_files if pdf not in existing_txt_files and pdf not in txt_error_files]
 len(pdfs_to_convert)
 
 pdf = pdfs_to_convert[0]
-# pdf
+pdf
 existing_pdf_file_path_strings = list(map(str, existing_pdf_file_paths))
 pdf_file_paths_to_convert = []
 for pdf in tqdm(pdfs_to_convert):
@@ -35,10 +44,9 @@ for pdf in tqdm(pdfs_to_convert):
     file_path = list(filter(pdf_re.match, existing_pdf_file_path_strings))[0]
     pdf_file_paths_to_convert.append(file_path)
 
-
 # # after running this loop, move all txt files from pdf folders into txt folders
-# file_path = pdf_file_paths_to_convert[432]
-# file_path
+file_path = pdf_file_paths_to_convert[432]
+file_path
 
 def pdf_to_txt(file_path):
     pdf_stem = Path(file_path).stem
--- code/cpet_articles/tidying/fix_bad_doi_filenames.py
+++ code/cpet_articles/tidying/fix_bad_doi_filenames.py
@@ -2,37 +2,27 @@ from pathlib import Path
 import re
 import shutil
 
-pdf_file_paths = list(Path('data/cpet_articles/full_texts/pdfs').rglob('*.pdf'))
-# pdf_files = [path.stem for path in pdf_file_paths]
+# we're changing bad characters to _._
+# e.g. 0008-6363(95)00199-9 becomes 0008-6363_._95_._00199-9
+
+pdf_file_paths = list(Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/pdfs').rglob('*.pdf'))
+pdf_files = [path.stem for path in pdf_file_paths]
 # I'm not sure I can fix this with shutil since it can't figure out how to use
 # forward slashes / in file names. It think it's a folder separator
 
-split_error_re = re.compile(r"', '")
-split_errors = list(filter(split_error_re.search, list(map(str, pdf_file_paths))))
-len(split_errors)
-split_errors[0]
-
 test_path = 'data/cpet_articles/full_texts/pdfs/unpaywall_oa/0008-6363(95)00199-9.pdf'
-parens_re = re.compile(r'(?=/.{0,100}/).*[()]+.*(?<=\.pdf)')
-# parens_re.search(test_path).group()
+bad_chars_re = re.compile(r"""((?=/full_texts/pdfs/.{0,100}/).*[()\\*,"': ?;<>]+.*(?<=\.pdf))""")
+bad_chars_re.search(test_path).group()
+bad_names = list(filter(bad_chars_re.search, list(map(str, pdf_file_paths))))
+len(bad_names)
+# good_names = [re.sub(r"""([()\\*,"': ?;<>])""", '_._', name) for name in bad_names]
+# good_names
 
-parens = list(filter(parens_re.search, list(map(str,pdf_file_paths))))
-len(parens)
-for p in parens:
-    print(p)
-
-pdf_path = split_errors[0]
-
-for pdf_path in pdf_file_paths:
-    if re.search(r"', '", str(pdf_path)):
-        source = pdf_path
-        dest = re.sub(r"', '", '/', source)
-        shutil.move(source, dest)
-
-
-if re.search(r"', '", str(pdf_path)):
-    source = pdf_path
-    dest = re.sub(r"', '", '', source)
-
-
-shutil.move(source, dest)
+for name in bad_names:
+    try:
+        path = Path(name)
+        new_stem = re.sub(r"""([()\\*,"': ?;<>])""", '_._', path.stem)
+        new_name = path.parent / str(new_stem + '.pdf')
+        shutil.move(src=name, dst=new_name)
+    except Exception as e:
+        print(e)
