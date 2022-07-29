b28738a6ef5c8bc43112bb19fa9e8e08a9c2f51c
Anton Hesse <ahesse2567@gmail.com>
2022-07-29 16:07:48 -0500

    Converted most epubs to txt

--- code/cpet_articles/tidying/convert_epubs_to_txt.py
+++ code/cpet_articles/tidying/convert_epubs_to_txt.py
@@ -1,34 +1,23 @@
 from pathlib import Path
 from epub2txt import epub2txt
+import pandas as pd
 
 proj_folder_path = Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review')
 epub_folder = proj_folder_path / 'data' / 'cpet_articles' / 'full_texts' / 'epubs'
 epub_paths = list(epub_folder.glob('*.epub'))
-path = epub_paths[12]
+
 txt_folder = proj_folder_path / 'data' / 'cpet_articles' / 'full_texts' # / 'txts'
+log =[]
 for path in epub_paths:
-    res = epub2txt(path)
-    fname = str(txt_folder / (path.stem + '.txt'))
-    with open(fname, 'w') as f:
-        f.write(res)
+    out = {'doi_suffix': path.stem}
+    try:
+        res = epub2txt(path)
+        fname = str(txt_folder / (path.stem + '.txt'))
+        with open(fname, 'w') as f:
+            f.write(res)
+    except Exception as e:
+        out.update({'error': e})
+    log.append(out)
 
-# txt_folder = proj_folder_path / 'data' / 'cpet_articles' / 'full_texts'
-# fname = str(txt_folder / (path.stem + '.txt'))
-# with open(fname, 'w') as f:
-#     f.write(res)
-
-
-# res = epub2txt(epub_paths[0])
-# res
-
-# book = epub.read_epub(epub_paths[22])
-# book
-
-# list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
-
-# text = []
-# for doc in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
-#     text.append(str(doc.content))
-
-# text = sum(text)
-# text[0]
+log_df = pd.DataFrame(log)
+log_df[~log_df['error'].isna()]
