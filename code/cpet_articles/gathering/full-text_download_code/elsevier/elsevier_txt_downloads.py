009ffd79d9628eb758eaabcaaaa4aa764874b24d
Anton Hesse <ahesse2567@gmail.com>
2022-08-03 14:02:25 -0500

    Retrying Elsevier downloads
    After fixing file names and reconciling remaining articles, it made sense to run this again.

--- code/cpet_articles/gathering/full-text_download_code/elsevier/elsevier_txt_downloads.py
+++ code/cpet_articles/gathering/full-text_download_code/elsevier/elsevier_txt_downloads.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append('code/cpet_articles/gathering/full-text_download_code/')
-from helper_funcs.articles import get_current_full_texts
+sys.path.append('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/code/cpet_articles/gathering/full-text_download_code/')
+from helper_funcs.articles import get_current_full_texts, get_doi_suffix
 import pandas as pd
 import requests
 import json
@@ -17,14 +17,11 @@ from pathlib import Path
 current_full_texts = get_current_full_texts()
 
 all_articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv')
-
-re_doi_suffix = re.compile(r'(?<=\d/).*')
-all_articles['doi_suffix'] = all_articles['doi'].apply(lambda x: re_doi_suffix.search(x).group())
-full_texts_to_download = [x for x in all_articles['doi_suffix'].tolist() if x not in current_full_texts]
+all_articles['doi_suffix'] = all_articles['doi'].apply(lambda x: get_doi_suffix(x))
+full_texts_to_download = [x for x in tqdm(all_articles['doi_suffix'].tolist()) if x not in current_full_texts]
 
 remaining_articles = pd.merge(pd.DataFrame({'doi_suffix': full_texts_to_download}), all_articles, how='inner', on='doi_suffix')
-articles = remaining_articles[(all_articles['publisher'] == 'Elsevier BV') & \
-    (remaining_articles['is_oa'] == False)].drop_duplicates().reset_index(drop=True)
+articles = remaining_articles[remaining_articles['publisher'] == 'Elsevier BV'].drop_duplicates().reset_index(drop=True)
 articles.shape
 
 # Load api key
@@ -40,8 +37,13 @@ elsevier_params = {
     'view': 'FULL'
 }
 elsevier_headers ={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'}
-folder = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/txts/eslevier_non_oa_txts'
+folder = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/txts'
 file_ext = 'txt'
+
+"""
+n = random.randint(0, len(articles))
+row = articles.loc[n,:]
+"""
 
 log = []
 for idx, row in tqdm(articles.iterrows(), total=len(articles)):
@@ -51,7 +53,7 @@ for idx, row in tqdm(articles.iterrows(), total=len(articles)):
 
     try:
         r = requests.get(url=doi_url, params=elsevier_params, headers=elsevier_headers,
-        allow_redirects=True, verify=True)
+            allow_redirects=True, verify=True)
         out.update({'publisher_status_code': r.status_code})
 
         if r.status_code == 200:
@@ -74,22 +76,27 @@ log_df['publisher_status_code'].value_counts()
 error_df = log_df[(log_df['publisher_status_code'] != 200) | (~log_df['error'].isna())]
 
 merge = pd.merge(error_df, articles, how='inner', on='doi')
-merge.to_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/elsevier_non_oa_articles_status_codes.csv',
+merge.to_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/elsevier_status_codes.csv',
     index=False)
 
-
-# n = random.randint(0, len(elsevier_non_oa_articles))
-# # doi = elsevier_non_oa_articles.loc[0,'doi']
+"""
+n = random.randint(0, len(articles))
+doi = articles.loc[n,'doi']
 # doi = '10.1378/chest.97.1.12'
-# doi_url = 'https://api.elsevier.com/content/article/doi/' + doi
+doi_url = 'https://api.elsevier.com/content/article/doi/' + doi
 
-# r = requests.get(url=doi_url, params=elsevier_params, headers=elsevier_headers,
-# allow_redirects=True, verify=True)
-# r.status_code
-# # r.raw
+r = requests.get(url=doi_url, params=elsevier_params,headers=elsevier_headers,
+    allow_redirects=True,verify=True)
+r.status_code
 
-# doi_suffix = str(doi.split('/')[1:]).strip("[']")
-# filename = f'{folder}/{doi_suffix}{file_ext}'
+doi_suffix = get_doi_suffix(doi)
+filename = f'{folder}/{doi_suffix}.{file_ext}'
 
-# with open(filename, mode='wb') as f:
-#     f.write(r.content)
+with open(filename, mode='wb') as f:
+    f.write(r.content)
+"""
+
+
+
+
+
