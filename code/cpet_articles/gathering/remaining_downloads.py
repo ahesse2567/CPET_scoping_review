23f1db50f734aac656b482c55d6990cea4a67b15
Anton Hesse <ahesse2567@gmail.com>
2022-08-03 14:02:52 -0500

    Code to create manual downloads csv

--- code/cpet_articles/gathering/remaining_downloads.py
+++ code/cpet_articles/gathering/remaining_downloads.py
@@ -1,31 +1,26 @@
 from pathlib import Path
 import pandas as pd
 import re
+from tqdm import tqdm
+import sys
+sys.path.append('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/code/cpet_articles/gathering/full-text_download_code/')
+from helper_funcs.articles import get_current_full_texts, get_doi_suffix
 
-pdf_file_paths = list(Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/pdfs').glob('*.pdf'))
-pdfs = [path.stem for path in pdf_file_paths]
+current_full_texts = get_current_full_texts()
+all_articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv')
+all_articles['doi_suffix'] = all_articles['doi'].apply(lambda x: get_doi_suffix(x))
+full_texts_to_download = [x for x in tqdm(all_articles['doi_suffix'].tolist()) if x not in current_full_texts]
 
-txt_file_paths = list(Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/txts').glob('*.txt'))
-txts = [path.stem for path in txt_file_paths]
-
-epub_file_paths = list(Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/epubs').glob('*.epub'))
-epubs = [path.stem for path in epub_file_paths]
-
-full_texts = list(set(pdfs + txts + epubs)) # use set() to remove duplicates
-
-articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv')
-
-re_doi_suffix = re.compile(r'(?<=\d/).*')
-articles['doi_suffix'] = articles['doi'].apply(lambda x: re_doi_suffix.search(x).group())
-full_texts_to_download = [x for x in articles['doi_suffix'].tolist() if x not in full_texts]
-len(full_texts_to_download)
-
-remaining_articles = pd.merge(pd.DataFrame({'doi_suffix': full_texts_to_download}), articles, how='inner', on='doi_suffix')
+remaining_articles = pd.merge(pd.DataFrame({'doi_suffix': full_texts_to_download}), all_articles, how='inner', on='doi_suffix')
+remaining_articles.shape
 print(remaining_articles['publisher'].value_counts()[0:40])
-print('\n')
+print()
 print(f'Total remaining articles to download: {remaining_articles.shape[0]}')
 
-manual_download_publishers = remaining_articles['publisher'].value_counts()[remaining_articles['publisher'].value_counts() < 100].index.to_list()
-manual_download_df = remaining_articles[remaining_articles['publisher'].isin(manual_download_publishers)].drop_duplicates().reset_index(drop=True)
-manual_download_df.to_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/manual_downloads.csv',
-index=False)
+choice = input('Would you like to rerun articles to download manually? (y/n) ')
+if choice == 'y':
+    manual_download_publishers = remaining_articles['publisher'].value_counts()[remaining_articles['publisher'].value_counts() < 100].index.to_list()
+    manual_download_df = remaining_articles[remaining_articles['publisher'].isin(manual_download_publishers)].drop_duplicates().reset_index(drop=True)
+    manual_download_df.to_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/manual_downloads.csv',
+    index=False)
+
