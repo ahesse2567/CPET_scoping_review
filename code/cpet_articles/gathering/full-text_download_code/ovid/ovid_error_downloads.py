ca830b0c478e1085bb7d9afcd84e40f977572dd2
Anton Hesse <ahesse2567@gmail.com>
2022-08-09 14:39:26 -0500

    ovid, aps, and oxford download udpates

--- code/cpet_articles/gathering/full-text_download_code/ovid/ovid_error_downloads.py
+++ code/cpet_articles/gathering/full-text_download_code/ovid/ovid_error_downloads.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.chrome.options import Options as FirefoxOptions
 # firefox_options = FirefoxOptions()
 # firefox_options.add_argument("--headless")
@@ -19,7 +20,7 @@ from helper_funcs.articles import *
 
 def find_buttons(driver):
     PDF_re = re.compile('PDF')
-    outer_download_button = driver.find_element(By.CLASS_NAME, 'icon-pdf').find_element(By.XPATH, '../../..')
+    outer_download_button = driver.find_element(By.XPATH, "//i[@class='icon-pdf']").find_element(By.XPATH, '../../..')
     inner_buttons = outer_download_button.find_elements(By.TAG_NAME, 'i')
     button_html = [i.find_element(By.XPATH, '..').get_attribute('innerHTML') for i in inner_buttons]
     pdf_button_list = [PDF_re.search(html) for html in button_html]
@@ -38,6 +39,36 @@ def find_buttons(driver):
     
     return outer_download_button, inner_download_button, button_type
 
+def check_retry_later_popup(driver):
+    if len(driver.find_elements(By.XPATH, "//button[text()='Retry later']")) > 0:
+        retry_later_button = driver.find_element(By.XPATH, "//button[text()='Retry later']")
+        retry_later_button.click()
+
+def check_subscription(driver, log_dict):
+    if len(driver.find_elements(By.XPATH, "//h3[text()='Not a Subscriber?']")) > 0:
+        log_dict.update({'subscribed': False})
+        return log_dict
+    else:
+        log_dict.update({'subscribed': True})
+        return log_dict
+
+def correct_page(driver, sleep_time=5):
+    if len(driver.find_elements(By.XPATH, "//span[text()='There are 0 results for: ']")) > 0:
+        search_bar = driver.find_element(By.XPATH, "//input[@id='searchTermInput']")
+        search_bar.click()
+        search_bar.clear()
+        search_bar.send_keys(doi)
+        search_bar.send_keys(Keys.RETURN)
+        time.sleep(sleep_time)
+    if len(driver.find_elements(By.XPATH, "//span[contains(text(), 'Showing 1 result for: ')]")) > 0:
+        search_ovid(driver)
+
+def search_ovid(driver):
+    article_link = driver.find_element(By.XPATH, f"//a[contains(text(), '{row['title']}')]")
+    article_link.click()
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+
 current_full_texts = get_current_full_texts()
 
 all_articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv')
@@ -46,6 +77,7 @@ full_texts_to_download = [x for x in tqdm(all_articles['doi_suffix'].tolist()) i
 remaining_articles = pd.merge(pd.DataFrame({'doi_suffix': full_texts_to_download}), all_articles, how='inner', on='doi_suffix')
 
 articles = remaining_articles[remaining_articles['publisher'] == 'Ovid Technologies (Wolters Kluwer Health)'].drop_duplicates().reset_index(drop=True)
+articles = articles.sample(frac=1).reset_index(drop=True)
 articles.shape
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0'}
@@ -67,14 +99,19 @@ for idx, row in tqdm(articles.iterrows(), total=articles.shape[0]):
         out.update({'doi_redirect_SC': doi_resp.status_code})
         if doi_resp.status_code == 200:
             driver.get(doi_resp.url)
-            time.sleep(3) # seems to let advertisement close
+            time.sleep(2) # seems to let advertisement close
+            check_retry_later_popup(driver)
+            correct_page(driver)
+            out = check_subscription(driver, out)
+            if out['subscribed'] == False:
+                continue
             outer_download_button, inner_download_button, button_type = find_buttons(driver)
             outer_download_button.location_once_scrolled_into_view
             time.sleep(1)
             outer_download_button.click()
             inner_download_button.click()
             if button_type == 'pdf':
-                time.sleep(15) # allow for download time
+                time.sleep(20) # allow for download time
                 close_extra_tabs(driver)
                 pdfs_in_downloads_paths = list(Path('/Users/antonhesse/Downloads').glob('*.pdf'))
                 if len(pdfs_in_downloads_paths) > 1:
@@ -104,7 +141,9 @@ for idx, row in tqdm(articles.iterrows(), total=articles.shape[0]):
 driver.quit()
         
 log_df = pd.DataFrame(log)
-error_df = log_df[(~log_df['error'].isnull()) | (log_df['doi_redirect_SC'] != 200) | (log_df['full_text_SC'] != 200)]
+# error_df = log_df[(~log_df['error'].isnull()) | (log_df['doi_redirect_SC'] != 200) | (log_df['full_text_SC'] != 200)]
+log_df.to_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/ovid_errors.csv',
+index=False)
 
 # idx
 # sometimes it might be scrolling too far down
--- code/cpet_articles/gathering/full-text_download_code/oxford_scraping.py
+++ code/cpet_articles/gathering/full-text_download_code/oxford_scraping.py
@@ -10,16 +10,13 @@ from tqdm import tqdm
 from pathlib import Path
 import re
 import sys
-sys.path.append('code/cpet_articles/gathering/full-text_download_code/')
-from helper_funcs.articles import get_current_full_texts, download_pdf
+sys.path.append('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/code/cpet_articles/gathering/full-text_download_code/')
+from helper_funcs.articles import *
 
 current_full_texts = get_current_full_texts()
 
 all_articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv')
-
-all_articles['doi_suffix'] = all_articles['doi'].apply(lambda x: str(x.split('/', 1)[1:]).strip("[']"))
-all_articles['doi_suffix'] = all_articles['doi_suffix'].apply(lambda x: re.sub(r"""([()\\*,"': /?;<>])""", '_._', x))
-
+all_articles['doi_suffix'] = all_articles['doi'].apply(lambda x: get_doi_suffix(x))
 full_texts_to_download = [x for x in tqdm(all_articles['doi_suffix'].tolist()) if x not in current_full_texts]
 
 remaining_articles = pd.merge(pd.DataFrame({'doi_suffix': full_texts_to_download}), all_articles, how='inner', on='doi_suffix')
