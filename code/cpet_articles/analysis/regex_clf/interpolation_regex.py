5e237f4b147e650c76cae71996aba6cbc47f46ca
Anton Hesse <ahesse2567@gmail.com>
2022-09-21 12:53:38 -0500

    Predicting data processing and gas data
    Predicting gas data won't work well because almost all articles that reference BBB in some way DID measure gas data.
    
    Detecting data processing text has some potential, but the RF classifier is only at 65% accuracy right now...regex will probably be better.

--- code/cpet_articles/analysis/regex_clf/interpolation_regex.py
+++ code/cpet_articles/analysis/regex_clf/interpolation_regex.py
@@ -6,6 +6,7 @@ import sys
 helper_funcs_path = Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/code/cpet_articles/analysis/')
 sys.path.append(str(helper_funcs_path))
 from helper_funcs.text_analysis import read_raw_text
+tqdm.pandas()
 
 def get_doi_suffix(doi):
     doi_suffix = str(doi.split('/', 1)[1:]).strip("[']")
@@ -14,3 +15,29 @@ def get_doi_suffix(doi):
     return doi_suffix
 
 txt_file_paths = list(Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/txts').rglob('*.txt'))
+
+bbb_articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/text_analysis/bbb_articles.csv')
+bbb_articles['doi_suffix'] = bbb_articles['doi'].apply(lambda x: get_doi_suffix(x))
+
+bbb_article_paths = [path for path in txt_file_paths if path.stem in bbb_articles['doi_suffix'].to_list()]
+
+raw_text = []
+for path in tqdm(bbb_article_paths):
+    try:
+        rt = read_raw_text(path)
+        raw_text.append(rt)
+    except FileNotFoundError as e:
+        print(e)
+
+txt_files = [path.stem for path in bbb_article_paths]
+text_df = pd.DataFrame({
+    'doi_suffix': txt_files,
+    'path': bbb_article_paths,
+    'text': raw_text})
+
+interpolate_re = re.compile(r'interpolat')
+
+text_df['interpolate'] = text_df['text'].progress_apply(lambda x: True if interpolate_re.search(x) is not None else False)
+text_df['interpolate'].value_counts()
+
+text_df[text_df['interpolate']]
