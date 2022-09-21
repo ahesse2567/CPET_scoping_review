e36fecdf2f5a48a4e68802dac31207f309c1e443
Anton Hesse <ahesse2567@gmail.com>
2022-09-21 12:53:53 -0500

    Random small updates

--- code/cpet_articles/gathering/update_downloaded_articles.py
+++ code/cpet_articles/gathering/update_downloaded_articles.py
@@ -4,7 +4,7 @@ import sys
 helper_func_folder_path = Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/code/cpet_articles/gathering/full-text_download_code/')
 
 sys.path.append(str(helper_func_folder_path))
-from helper_funcs.articles import get_doi_suffix
+from helper_funcs.articles import get_doi_suffix, get_current_full_texts
 
 current_full_texts = get_current_full_texts()
 current_full_texts_df = pd.DataFrame({'doi_suffix': current_full_texts})
