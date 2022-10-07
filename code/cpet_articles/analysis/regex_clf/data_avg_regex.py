abba48f2f31cbea78395177ed3f2c2b6239e1019
Anton Hesse <ahesse2567@gmail.com>
2022-10-07 11:35:02 -0500

    Update data_avg_regex.py
    Extracted surrounding text. Next step is to see if surrounding text references VO2 or pulmonary variables in any way

--- code/cpet_articles/analysis/regex_clf/data_avg_regex.py
+++ code/cpet_articles/analysis/regex_clf/data_avg_regex.py
@@ -8,11 +8,40 @@ sys.path.append(str(helper_funcs_path))
 from helper_funcs.text_analysis import read_raw_text
 tqdm.pandas()
 
+# me trying to figure out my weird import errors if I try and do this from another machine
+# https://napuzba.com/a/import-error-relative-no-parent
+# https://codesource.io/how-to-fix-the-importerror-attempted-relative-import-with-no-known-parent-package-in-python/
+# https://stackoverflow.com/questions/60593604/importerror-attempted-relative-import-with-no-known-parent-package
+# helper_funcs_path = Path('code/cpet_articles/analysis/')
+# import os, sys
+# sys.path.append("..")
+# print(os.getcwd())
+# # sys.path.append(str(helper_funcs_path))
+# from ..helper_funcs.text_analysis import read_raw_text
+# tqdm.pandas()
+
+# print("It worked")
+
 def get_doi_suffix(doi):
     doi_suffix = str(doi.split('/', 1)[1:]).strip("[']")
     doi_suffix = re.sub(r"""([()\\*,"': /?;<>])""", '_._', doi_suffix) # remove bad chars
     doi_suffix = re.sub(r'(_._){2,}', '_._', doi_suffix) # remove multiple sequences of _._
     return doi_suffix
+
+def reorder_columns(dataframe, col_name, position):
+    """Reorder a dataframe's column.
+    Args:
+        dataframe (pd.DataFrame): dataframe to use
+        col_name (string): column name to move
+        position (0-indexed position): where to relocate column to
+    Returns:
+        pd.DataFrame: re-assigned dataframe
+    """
+    temp_col = dataframe[col_name]
+    dataframe = dataframe.drop(columns=[col_name])
+    dataframe.insert(loc=position, column=col_name, value=temp_col)
+    return dataframe
+
 
 txt_file_paths = list(Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/txts').rglob('*.txt'))
 
@@ -53,9 +82,9 @@ time_bin_avg_sec_re = re.compile(r'''(
     )''', re.IGNORECASE | re.DOTALL | re.VERBOSE)
 
 # I think I might first search by units in seconds, and later in units that include minutes
-text = text_df.loc[text_df['doi_suffix'] == 'mss.0000000000001429']
-text = text.lower()
-time_bin_avg_sec_re.findall(text)
+# text = text_df.loc[text_df['doi_suffix'] == 'mss.mss.0000000000001353']
+# text = text.lower()
+# time_bin_avg_sec_re.findall(text)
 
 text_df['time_bin_avg_sec'] = text_df['text'].progress_apply(lambda x: time_bin_avg_sec_re.findall(x) if len(time_bin_avg_sec_re.findall(x)) > 0 else False)
 text_df['time_bin_avg_sec'][0:10]
@@ -99,12 +128,49 @@ extract_avgs(text_list)
 text_df['time_bin_avg_nums'] = text_df['time_bin_avg_sec'].apply(lambda x: extract_avgs(x))
 text_df['time_bin_avg_nums'][0]
 
+import random
+n = random.randint(0, text_df.shape[0])
+single_row = text_df.loc[0,:]
+
+
+def get_surrounding_text(phrase, text, chars=100):
+    phrase = re.escape(phrase)
+    surrounding_text_re = re.compile(fr'.{{0,{chars}}}{phrase}.{{0,{chars}}}', re.IGNORECASE | re.DOTALL)
+    if surrounding_text_re.search(text):
+        return surrounding_text_re.search(text).group()
+    else:
+        return None
+
+
+row = text_df.loc[22,:]
+row
+
+surrounding_text = []
+for idx, row in tqdm(text_df.iterrows(), total=text_df.shape[0]):
+    temp_list = []
+    if isinstance(row['time_bin_avg_nums'], list):
+        for ph, num in row['time_bin_avg_nums']:
+            temp = get_surrounding_text(phrase=ph, text=row['text'])
+            temp_list.append(temp)
+        surrounding_text.append(temp_list)
+    else:
+        surrounding_text.append(False)
+    
+
+for i in range(20):
+    print(surrounding_text[i])
+
+text_df['time_bin_surrounding_text'] = surrounding_text
+
+
 manual_text_analysis_path = Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/text_analysis/Manual text analysis - Data.csv')
 manual_text_analysis_df = pd.read_csv(manual_text_analysis_path, dtype='str')
 
 # copy this into the Google Sheet
-merge_df = pd.merge(manual_text_analysis_df, text_df[['doi_suffix', 'time_bin_avg_nums']], how='outer', on='doi_suffix').drop_duplicates(subset='doi_suffix')
+merge_df = pd.merge(manual_text_analysis_df, text_df[['doi_suffix', 'time_bin_avg_nums', 'time_bin_surrounding_text']], how='outer', on='doi_suffix').drop_duplicates(subset='doi_suffix')
 merge_df['doi_suffix'] = merge_df['doi_suffix'].astype('str')
+merge_df = reorder_columns(merge_df, 'time_bin_avg_nums', position=10)
+merge_df = reorder_columns(merge_df, 'time_bin_surrounding_text', position=11)
 merge_df.to_clipboard(index=False)
 # rolling breath average
 # if breath number appears BEFORE "smoothing, rolling, etc."
