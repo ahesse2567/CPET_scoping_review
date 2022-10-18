172fd29b4b096453eb3ca1ff37da7d4c597ec79b
Anton Hesse <ahesse2567@gmail.com>
2022-10-18 15:13:12 -0500

    SD regex, non-capturing groups
    Non-capturing groups seems like what I need because I want to return the entire string, not the groups from re.findall()

--- code/cpet_articles/analysis/regex_clf/outlier_re.py
+++ code/cpet_articles/analysis/regex_clf/outlier_re.py
@@ -37,43 +37,52 @@ text_df = pd.DataFrame({
     'text': raw_text})
 
 def find_outlier_text(text):
-    cough_re = re.compile(r'\s(cough(ing)?)', re.IGNORECASE) # cough is always included
+    cough_re = re.compile(r'\s(?:cough(?:ing)?)', re.IGNORECASE) # cough is always included
     # at least one other term is required to accompany 'cough' in order to reference outliers
-    weird_breath_re = re.compile(r'\s(swallow|sigh|talk|(throat.{0,2}clear|clear(ing)?.{0,2}throat))', re.DOTALL| re.IGNORECASE)
+    weird_breath_re = re.compile(r'\s(?:swallow|sigh|talk|(?:throat.{0,2}clear|clear(?:ing)?.{0,2}throat))', re.DOTALL| re.IGNORECASE)
 
     error_synosyms_re = re.compile(r'\serrant|\saberrant', re.IGNORECASE) # these almost always refer to breaths
     # outlying, outlier, deviating, erroneous. These options sometimes appear but aren't reliable
-    local_mean_re = re.compile(r'\slocal.{0,2}(mean|average)', re.DOTALL | re.IGNORECASE) # 'local mean' always refers to outliers
-    # sd_re = re.compile(r'') # may need to search for terms related to finding standard deviations
-    # \W(three|four|3|4).{0,2}(sd|standard.{0,2}deviations?)\W
+    local_mean_re = re.compile(r'\slocal.{0,2}(?:mean|average)', re.DOTALL | re.IGNORECASE) # 'local mean' always refers to outliers
+    
+    # search for terms related to finding standard deviations
+    # (?:breath|v.{0,3}o2).{0,175}?[^a-zA-Z0-9_.](?:three|four|3|4)\D{0,2}(?:sd|standard.{0,2}deviations?)\W|[^a-zA-Z0-9_.](?:three|four|3|4)\D{0,2}(?:sd|standard.{0,2}deviations?)\W(?:breath|v.{0,3}o2).{0,175}?
+    sd_re = re.compile(r'''(
+        (?:breath|v.{0,3}o2).{0,175}?[^a-zA-Z0-9_.](?:three|four|3|4)\D{0,2}(?:sd|standard.{0,2}deviations?)\W|
+        [^a-zA-Z0-9_.](?:three|four|3|4)\D{0,2}(?:sd|standard.{0,2}deviations?)\W(?:breath|v.{0,3}o2).{0,175}?
+    )''', re.IGNORECASE | re.DOTALL | re.VERBOSE)
+
     # v?\W{0,3}o2\b.{0,300}?out.{0,50}prediction.{0,2}(interval|band) |prediction.{0,2}(interval|band).{0,50}out.{0,300}?v\W{0,3}o2\b
     prediction_interval_re = re.compile(r'''(
-        v?\W{0,3}o2\b.{0,300}?out.{0,50}prediction.{0,2}(interval|band)
-        |prediction.{0,2}(interval|band).{0,50}out.{0,300}?v\W{0,3}o2\b)''', re.DOTALL | re.IGNORECASE | re.VERBOSE)
+        v?\W{0,3}o2\b.{0,300}?out.{0,50}prediction.{0,2}(?:interval|band)
+        |prediction.{0,2}(?:interval|band).{0,50}out.{0,300}?v\W{0,3}o2\b)''', re.DOTALL | re.IGNORECASE | re.VERBOSE)
     # out.{0,20} needs to incldue 'out' as in outlier, outside, etc
-    if all([cough_re.search(text), weird_breath_re.search(text)]) or any([error_synosyms_re.search(text), local_mean_re.search(text), prediction_interval_re.search(text)]):
+    cough_breath_bool = all([cough_re.search(text), weird_breath_re.search(text)])
+    other_bool = any([error_synosyms_re.search(text), local_mean_re.search(text),prediction_interval_re.search(text),sd_re.search(text)])
+
+    if cough_breath_bool or other_bool:
         mo_list = [
-            cough_re.search(text),
-            weird_breath_re.search(text),
-            error_synosyms_re.search(text),
-            local_mean_re.search(text),
-            prediction_interval_re.search(text)]
-        out = [mo.group() for mo in mo_list if mo]
+            cough_re.findall(text),
+            weird_breath_re.findall(text),
+            error_synosyms_re.findall(text),
+            local_mean_re.findall(text),
+            prediction_interval_re.findall(text),
+            sd_re.findall(text)]
+        out = [mo for mo in mo_list if mo] # change to [mo[0] for mo in mo_list if mo] to only get string, not the list?
         return out
     else:
         return False
 
-text = text_df.loc[text_df['doi_suffix'] == 'japplphysiol.01040.2014',:]['text'].values[0]
-
+# import random
+# n = random.randint(0, text_df.shape[0])
+# text = text_df.loc[n,:]['text']
+text = text_df.loc[text_df['doi_suffix'] == 'japplphysiol.90357.2008',:]['text'].values[0]
 find_outlier_text(text)
 
-
-# prediction_interval_re = re.compile(r'prediction.{0,2}(interval|band)', re.DOTALL | re.IGNORECASE)
-# text_df['prediction_int'] = text_df['text'].progress_apply(lambda x: prediction_interval_re.search(x).group() if prediction_interval_re.search(x) else False)
-# text_df[text_df['prediction_int'] != False]
-
 text_df['outlier_terms'] = text_df['text'].progress_apply(lambda x: find_outlier_text(x))
-text_df['outlier_terms'].value_counts()
+# text_df['outlier_terms'].value_counts()
+
+text_df[text_df['outlier_terms'] != False]
 
 # load then merge with manual analysis df
 manual_text_analysis_path = Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/text_analysis/Manual text analysis - Data.csv')
@@ -83,7 +92,7 @@ merge_df = pd.merge(manual_text_analysis_df, text_df[['doi_suffix', 'outlier_ter
 merge_df['doi_suffix'] = merge_df['doi_suffix'].astype('str')
 merge_df['outlier_terms'].to_clipboard(index=False)
 
-merge_df.loc[merge_df['doi_suffix'] == 'ajpregu.00015.2019',:]
+# merge_df.loc[merge_df['doi_suffix'] == 'ajpregu.00015.2019',:]
 
 
 # lump different cutoff amounts using the empiricle rule (68, 95, 99%)
