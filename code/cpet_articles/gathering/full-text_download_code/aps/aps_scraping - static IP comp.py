43ec9f54a555b7547563f054da36488be02b207a
ahesse2567 <ahesse2567@gmail.com>
2022-10-18 10:06:44 -0500

    Update aps_scraping - static IP comp.py

--- code/cpet_articles/gathering/full-text_download_code/aps/aps_scraping - static IP comp.py
+++ code/cpet_articles/gathering/full-text_download_code/aps/aps_scraping - static IP comp.py
@@ -8,13 +8,17 @@ import random
 import time
 from tqdm import tqdm, trange
 from pathlib import Path
-import re
-import sys
-sys.path.append('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/code/cpet_articles/gathering/full-text_download_code/')
-from helper_funcs.articles import *
 import shutil
+import re
+# import sys
+# helper_funcs_path = Path(r'\code\cpet_articles\gathering\full-text_download_code\helper_funcs')
+# sys.path.append(helper_funcs_path)
 
-# As with Oxford(?), clicking the download button now sends the PDF directly to the down
+def get_doi_suffix(doi):
+    doi_suffix = str(doi.split('/', 1)[1:]).strip("[']")
+    doi_suffix = re.sub(r"""([()\\*,"': /?;<>])""", '_._', doi_suffix) # remove bad chars
+    doi_suffix = re.sub(r'(_._){2,}', '_._', doi_suffix) # remove multiple sequences of _._
+    return doi_suffix
 
 def click_links_landing_page(driver, sleep_time=1):
     pdf_dropdown_link = driver.find_element(By.XPATH, "//a[@data-db-target-for='pdfLinks']")
@@ -42,22 +46,34 @@ def wait(duration=60):
     for i in trange(duration):
         time.sleep(1)
 
-current_full_texts = get_current_full_texts()
+def close_extra_tabs(driver):
+    driver.switch_to.window(driver.window_handles[0])
+    parent_tab = driver.current_window_handle
+    for handle in driver.window_handles:
+        if handle != parent_tab:
+            driver.switch_to.window(handle)
+            driver.close()
+    driver.switch_to.window(driver.window_handles[0])
 
-all_articles = pd.read_csv('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/unpaywall/unpaywall_info.csv')
+downloaded_articles = pd.read_csv(str(Path('data/cpet_articles/unpaywall/downloaded_articles.csv')))
+# downloaded_articles['doi_suffix'] = downloaded_articles['doi'].apply(lambda x: get_doi_suffix(x))
+all_articles = pd.read_csv(str(Path('data/cpet_articles/unpaywall/unpaywall_info.csv')))
 all_articles['doi_suffix'] = all_articles['doi'].apply(lambda x: get_doi_suffix(x))
-full_texts_to_download = [x for x in tqdm(all_articles['doi_suffix'].tolist()) if x not in current_full_texts]
 
-remaining_articles = pd.merge(pd.DataFrame({'doi_suffix': full_texts_to_download}), all_articles, how='inner', on='doi_suffix')
-
+remaining_articles = all_articles[~all_articles['doi_suffix'].isin(downloaded_articles['doi_suffix'].to_list())]
 articles = remaining_articles[remaining_articles['publisher'] == 'American Physiological Society'].drop_duplicates().reset_index(drop=True)
-articles.shape
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0'}
-dest_folder = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/pdfs'
+dest_folder = Path('data/cpet_articles/full_texts/pdfs')
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 driver.implicitly_wait(1) # hopefully let's JS load correctly
+
+# uncomment if you want to test a random article
+# n = random.randint(0, articles.shape[0])
+# row = articles.loc[n, :]
+
+# As with Oxford(?), clicking the download button now sends the PDF directly to the down
 
 log = []
 for idx, row in tqdm(articles.iterrows(), total=articles.shape[0]):
@@ -79,16 +95,19 @@ for idx, row in tqdm(articles.iterrows(), total=articles.shape[0]):
             time.sleep(3) # might help with switching windows
             close_extra_tabs(driver)
             # move PDF from downloads to pdf folder
-            pdfs_in_downloads_paths = list(Path('/Users/antonhesse/Downloads').glob('*.pdf'))
+
+            downloads_folder_path = Path(re.escape(r'C:\Users\hesse151\Downloads'))
+            pdfs_in_downloads_paths = list(downloads_folder_path.glob('*.pdf'))
             if len(pdfs_in_downloads_paths) > 1:
                 for path in pdfs_in_downloads_paths:
                     Path.unlink(path)
             else:
                 doi_suffix = get_doi_suffix(doi)
-                new_path = '/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts/pdfs/' + doi_suffix + '.pdf'
+                new_folder = Path(re.escape(r'C:\Users\hesse151\Documents\CPET_scoping_review\data\cpet_articles\full_texts\pdfs'))
+                new_path = new_folder / (doi_suffix + '.pdf')
                 shutil.move(src=pdfs_in_downloads_paths[0], dst=new_path)
                 time.sleep(2)
-            wait(5) # wait so our IP address isn't blocked
+            wait(1) # The IP address will get blocked, but waiting is probably a good idea, anyway
     except Exception as e:
         print(e)
         out.update({'error': e})
