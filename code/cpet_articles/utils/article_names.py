cecbfa3f6169ca365e956fb74a1d1ec293cfd70e
ahesse2567 <ahesse2567@gmail.com>
2022-11-09 11:15:18 -0600

    better get_doi_suffix, download_pdf
    New code uses regular expressions instead of str.split() to get the doi suffix
    
    download_pdf now uses pathlib instead of re.sub to fix folder paths ending in '/' or not ending in '/'

--- code/cpet_articles/utils/article_names.py
+++ code/cpet_articles/utils/article_names.py
@@ -1,7 +1,7 @@
 import re
 
 def get_doi_suffix(doi):
-    doi_suffix = str(doi.split('/', 1)[1:]).strip("[']")
+    doi_suffix = re.sub(r'10.\d+/', '', doi)
     doi_suffix = re.sub(r"""([()\\*,"': /?;<>])""", '_._', doi_suffix) # remove bad chars
     doi_suffix = re.sub(r'(_._){2,}', '_._', doi_suffix) # remove multiple sequences of _._
     return doi_suffix
