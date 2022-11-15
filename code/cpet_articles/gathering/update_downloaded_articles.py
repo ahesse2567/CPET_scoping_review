ca6d9f400e6e2eb67f7b97918225763c2ecd389e
Anton Hesse <ahesse2567@gmail.com>
2022-11-15 09:03:18 -0600

    Better imports and updated downloaded articles

--- code/cpet_articles/gathering/update_downloaded_articles.py
+++ code/cpet_articles/gathering/update_downloaded_articles.py
@@ -1,10 +1,7 @@
 import pandas as pd
 from pathlib import Path
-import sys
-helper_func_folder_path = Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/code/cpet_articles/gathering/full-text_download_code/')
-
-sys.path.append(str(helper_func_folder_path))
-from helper_funcs.articles import get_doi_suffix, get_current_full_texts
+from code.cpet_articles.gathering.full_text_download_code.helper_funcs.articles import get_current_full_texts
+from code.cpet_articles.utils.article_names import get_doi_suffix
 
 current_full_texts = get_current_full_texts()
 current_full_texts_df = pd.DataFrame({'doi_suffix': current_full_texts})
