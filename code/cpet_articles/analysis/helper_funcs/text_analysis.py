adc39b31dc1d758d65bd061378048d4c6e4a8590
Anton Hesse <ahesse2567@gmail.com>
2022-11-28 16:03:00 -0600

    Create normalize_text() to go inside read_raw_text()
    read_raw_text() also checks if the file is empty. normalize_text() can be used outside of read_raw_text() now.

--- code/cpet_articles/analysis/helper_funcs/text_analysis.py
+++ code/cpet_articles/analysis/helper_funcs/text_analysis.py
@@ -4,6 +4,66 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 
+def read_raw_text(file_path):
+    # check file size to make sure the txt file actually has text
+    file_size = 0
+    while file_size == 0:
+        file_size = Path(file_path).stat().st_size
+        if file_size != 0: # check if conversion to txt didn't work
+            with open(str(file_path), 'r') as f:
+                text = f.read()
+        else:
+            print('Empty file, returning None')
+            return None
+    text = normalize_text(text)
+
+    return text
+
+def normalize_text(text):
+    text_lower = text.lower()
+    # remove excessive space characters
+    text_lower = re.sub(r' {2,}', ' ', text_lower)
+    # remove excessive newlines with potential space and tab characters
+    text_lower = re.sub(r'(?:[ \t]*\n[ \t]*){2,}', '\n', text_lower)
+    # change excessive newlines to single newline
+    text_lower = re.sub(r'\n{2,}', '\n', text_lower)
+    # remove end of line hyphenations. This works b/c lookarounds don't(?) 'consume' their characters
+    text_lower = re.sub(r'(?<=[a-zA-Z])-\n(?=[a-zA-Z])', '', text_lower)
+    # remove newline characters mid paragraph. These are obviously needed to format the pdf
+    # document, but we don't need them when anlyzing text.
+    text_lower = re.sub(r'(?<!\n)\n(?=\S)', ' ', text_lower)
+    # fix weird single-letter character spacing like P . M O G N O N I ()
+    text_lower = re.sub(r'(?<= \w) (?=\w )', '', text_lower)
+    # remove whitespace BEFORE punctuation marks
+    text_lower = re.sub(r'\s(?=[.,?!;:])', '', text_lower)
+    # fix errors related to parsi,ng V̇O2 (i.e. V with a dot above it?) # unsure if needed
+    # text_lower = re.sub(r'v\W{0,3}o2\b', 'vo2', re.IGNORECASE)
+    return text_lower
+
+def get_surrounding_text(phrase, text, chars=200):
+    # get surrounding text if it is near words like vo2, breath, or metabolic
+    phrase = re.escape(phrase) # prevent escape character issues
+
+    surrounding_text_re = re.compile(fr'''(.{{0,{chars}}}{phrase}.{{0,{chars}}}
+        )''', re.DOTALL | re.VERBOSE)
+
+    vo2_breath_re = re.compile(
+        r'(?:v.{0,2})?o2|breath|metaboli|respirat|gas|air|ventilat|pulmonary|(oxygen|o2).{0,2}(consumption|uptake)', re.DOTALL)
+    
+    if surrounding_text_re.search(text):
+        res = surrounding_text_re.findall(text)
+        res = [r for r in res if vo2_breath_re.search(r)]
+        if res:
+            return res
+        else:
+            return None
+    else:
+        return None
+
+def capitalize_substring(main_string, sub_string):
+    out = main_string.replace(sub_string, sub_string.upper())
+    return out
+
 def tokenize_file(file_path, mode = 'lemm'):
     # check file size to make sure the txt file actually has text
     file_size = 0
@@ -32,52 +92,3 @@ def tokenize_file(file_path, mode = 'lemm'):
         stemmed_words = [stemmer.stem(t) for t in filtered_tokens]
     
         return stemmed_words
-
-def read_raw_text(file_path):
-    # check file size to make sure the txt file actually has text
-    file_size = 0
-    while file_size == 0:
-        file_size = Path(file_path).stat().st_size
-        if file_size != 0: # check if conversion to txt didn't work
-            with open(str(file_path), 'r') as f:
-                text = f.read()
-        else:
-            print('Empty file, returning None')
-            return None
-    text_lower = text.lower()
-    
-    # remove end of line hyphenations. This works b/c lookarounds don't(?) 'consume' their characters
-    text_lower = re.sub(r'(?<=[a-zA-Z])-\n(?=[a-zA-Z])', '', text_lower)
-    # remove excessive space characters
-    text_lower = re.sub(r' {2,}', ' ', text_lower)
-    # remove excessive newlines with potential space and tab characters
-    text_lower = re.sub(r'(?: ?\n+\t?){2,}', ' ', text_lower)
-    # fix errors related to parsi,ng V̇O2 (i.e. V with a dot above it?)
-    # text_lower = re.sub(r'v\W{0,3}o2\b', 'vo2', re.IGNORECASE)
-
-    return text_lower
-
-
-def get_surrounding_text(phrase, text, chars=200):
-    # get surrounding text if it is near words like vo2, breath, or metabolic
-    phrase = re.escape(phrase) # prevent escape character issues
-
-    surrounding_text_re = re.compile(fr'''(.{{0,{chars}}}{phrase}.{{0,{chars}}}
-        )''', re.DOTALL | re.VERBOSE)
-
-    vo2_breath_re = re.compile(
-        r'(?:v.{0,2})?o2|breath|metaboli|respirat|gas|air|ventilat|pulmonary|(oxygen|o2).{0,2}(consumption|uptake)', re.DOTALL)
-    
-    if surrounding_text_re.search(text):
-        res = surrounding_text_re.findall(text)
-        res = [r for r in res if vo2_breath_re.search(r)]
-        if res:
-            return res
-        else:
-            return None
-    else:
-        return None
-
-def capitalize_substring(main_string, sub_string):
-    out = main_string.replace(sub_string, sub_string.upper())
-    return out
