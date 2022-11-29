87b60cd58a26b37781453d8ef1584d2c27ca12b6
Anton Hesse <ahesse2567@gmail.com>
2022-11-29 13:55:26 -0600

    Split data_processing_reporting into outliers + interpolation

--- code/cpet_articles/analysis/interpolation_reporting.R
+++ code/cpet_articles/analysis/interpolation_reporting.R
@@ -1,86 +1,21 @@
-# Analysis of how often papers report data processing procedures and what
-# those procedures are
-
 library(tidyverse)
 library(stringr)
 library(scales)
 
-
 text_data <- read_csv("data/cpet_articles/text_analysis/Manual text analysis - Data.csv",
-                      show_col_types = FALSE)
+                      show_col_types = FALSE) %>% 
+    janitor::clean_names()
 
+douglas_bag_mixing_chamber_articles <- read_csv(
+    "data/cpet_articles/text_analysis/Manual text analysis - DB or MC.csv",
+    show_col_types = FALSE)
+
+# load_bbb articles, removing potential douglas bag or mixing chamber articles
 bbb_articles <- read_csv("data/cpet_articles/text_analysis/bbb_articles.csv",
-                         show_col_types = FALSE)
-
-merge_df <- inner_join(text_data, bbb_articles, by = "doi_suffix") %>% 
-    janitor::clean_names() %>% 
-    mutate(outlier_limit = str_remove(outlier_limit, "(?<=SD).*"))
-
+                         show_col_types = FALSE) %>% 
+    distinct(doi_suffix, .keep_all = TRUE) %>% 
+    filter(!(doi_suffix %in% douglas_bag_mixing_chamber_articles$doi_suffix))
                                       
-
-############### OUTLIERS #################
-
-# count by outlier cutoff type
-merge_df %>% 
-    group_by(outlier_limit) %>% 
-    summarize(n = n())
-
-outlier_reporting_frequency_plot <- merge_df %>% 
-    count(outlier_limit) %>% 
-    mutate(pct = round(prop.table(n),4)) %>% 
-    mutate(outlier_limit = if_else(
-        is.na(outlier_limit), "Unspecified", str_to_title(outlier_limit))) %>% 
-    ggplot(aes(x = outlier_limit, y = n)) +
-    geom_col() +
-    geom_text(aes(label = scales::percent(pct)), vjust = -0.5) +
-    geom_text(aes(label = n), vjust = -2) +
-    xlab("Outlier Limit") +
-    ylab("Count") +
-    ylim(0,8000) +
-    theme_minimal() +
-    labs(caption = "Outlier cutoff reporting frequency. Data are expressed as counts and percentages.\nN = 7833.") +
-    theme(plot.caption = element_text(hjust=0))
-outlier_reporting_frequency_plot
-
-# pct outlier cutoff by type
-merge_df %>% 
-    select(outlier_limit) %>% 
-    drop_na() %>% 
-    group_by(outlier_limit) %>% 
-    summarize(n = n()) %>% 
-    mutate(freq = n / sum(n) * 100)
-
-pct_outlier_limits <- merge_df %>% 
-    select(outlier_limit) %>% 
-    drop_na() %>% 
-    group_by(outlier_limit) %>% 
-    summarize(n = n()) %>% 
-    mutate(pct = prop.table(n)) %>% 
-    mutate(outlier_limit = str_to_title(outlier_limit)) %>% 
-    ggplot(aes(x = outlier_limit, y = n)) +
-    geom_col() +
-    geom_text(aes(label = scales::percent(pct)), vjust = -0.5) +
-    geom_text(aes(label = n), vjust = -2) +
-    xlab("Outlier Limit") +
-    ylab("Count") +
-    ylim(0,200) +
-    theme_minimal() +
-    labs(caption = "Outlier cutoff frequencies. Data are expressed as counts and percentages.\nN = 330.") +
-    theme(plot.caption = element_text(hjust=0))
-pct_outlier_limits
-
-
-# count of articles that specify outlier removal
-merge_df %>% 
-    select(outlier_limit) %>% 
-    summarize_all(list(count_outlier_procedure_described = ~ sum(!is.na(.))))
-
-# percentage of articles that specify outlier removal
-merge_df %>% 
-    select(outlier_limit) %>% 
-    summarize_all(list(
-        pct_outlier_procedure_described = ~ sum(!is.na(.)) / length(.)*100))
-
 ############### INTERPOLATION #################
 
 # count of articles that specify interpolation procedures
