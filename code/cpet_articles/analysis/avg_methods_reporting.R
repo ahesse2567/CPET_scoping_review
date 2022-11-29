6e61c6fcf647614f6743ee5c836e8a2d8c8867dd
Anton Hesse <ahesse2567@gmail.com>
2022-11-29 13:55:57 -0600

    Math appears good w/ avg report, next graphs

--- code/cpet_articles/analysis/avg_methods_reporting.R
+++ code/cpet_articles/analysis/avg_methods_reporting.R
@@ -2,18 +2,146 @@ library(tidyverse)
 library(stringr)
 library(scales)
 
+text_data <- read_csv("data/cpet_articles/text_analysis/Manual text analysis - Avg.csv",
+                      show_col_types = FALSE) %>% 
+    janitor::clean_names()
 
-text_data <- read_csv("data/cpet_articles/text_analysis/Manual text analysis - Data.csv",
-                      show_col_types = FALSE)
+douglas_bag_mixing_chamber_articles <- read_csv(
+    "data/cpet_articles/text_analysis/Manual text analysis - DB or MC.csv",
+    show_col_types = FALSE)
 
+# load_bbb articles, removing potential douglas bag or mixing chamber articles
 bbb_articles <- read_csv("data/cpet_articles/text_analysis/bbb_articles.csv",
-                         show_col_types = FALSE)
+         show_col_types = FALSE) %>% 
+    distinct(doi_suffix, .keep_all = TRUE) %>% 
+    filter(!(doi_suffix %in% douglas_bag_mixing_chamber_articles$doi_suffix))
 
 merge_df <- inner_join(text_data, bbb_articles, by = "doi_suffix") %>% 
-    janitor::clean_names() %>% 
-    mutate(outlier_limit = str_remove(outlier_limit, "(?<=SD).*"))
+    select(colnames(text_data)) %>% 
+    rowwise() %>%
+    mutate(all_NA = all(is.na(
+        c(avg_type, avg_subtype, avg_amount,
+          avg_mos, avg_mean_type)
+    ))) %>% 
+    filter(!is.na(avg_text) | all_NA == FALSE) # there are a few articles
+# that excaped my regex. However, I still found that they had averaging methods
+# when reading them manually.
 
 ######## Averaging methods #################
+
+total_articles <- merge_df %>% 
+    distinct(doi_suffix, .keep_all = FALSE) %>% 
+    nrow()
+
+# initial estimate of how many articles have averaging methods
+# only considering if they have avg details or not
+
+merge_df %>%
+    distinct(doi_suffix, .keep_all = TRUE) %>%
+    mutate(has_avg_details = case_when(
+            avg_text == FALSE ~ FALSE,
+            avg_text != FALSE & !is.na(avg_text) ~ TRUE,
+            is.na(avg_text) & all_NA == FALSE ~ TRUE)) %>%
+    count(has_avg_details) %>%
+    ungroup() %>%
+    mutate(prop = prop.table(n))
+# so about 70% have averaging method details of some kind. This percentage will
+# drop because not all avg_text actually indicates avg_methods
+
+documented_details_tab <- merge_df %>% 
+    filter(documented == TRUE | done == TRUE | all_NA == FALSE)  %>% 
+    distinct(doi_suffix, .keep_all = TRUE) %>% 
+    count(all_NA) %>% 
+    ungroup() %>% 
+    mutate(prop = prop.table(n))
+documented_details_tab
+
+# unique articles WITH averaging details
+documented_has_avg_details <- documented_details_tab %>% 
+    filter(all_NA == FALSE) %>% 
+    select(n) %>% 
+    pull()
+
+# unique articles WITHOUT averaging details
+documented_no_avg_details <- documented_details_tab %>% 
+    filter(all_NA == TRUE) %>% 
+    select(n) %>% 
+    pull()
+
+total_documented <- documented_has_avg_details + documented_no_avg_details
+
+pct_documented_no_avg_details <- documented_details_tab %>% 
+    filter(all_NA == TRUE) %>% 
+    select(prop) %>% 
+    pull()
+
+documented_doi_suffixes <- merge_df %>% 
+    filter(documented == TRUE | done == TRUE | all_NA == FALSE)  %>% 
+    distinct(doi_suffix) %>% 
+    pull()
+
+undocumented_doi_suffixes <- merge_df %>%
+    filter(!(doi_suffix %in% documented_doi_suffixes)) %>% 
+    distinct(doi_suffix) %>% 
+    pull()
+
+total_undocumented <- total_articles - total_documented
+
+
+count_undocumented_no_avg_text <- merge_df %>%
+    filter(doi_suffix %in% undocumented_doi_suffixes) %>% 
+    filter(avg_text == FALSE) %>%
+    nrow()
+
+count_undocumented_with_avg_text <- merge_df %>% 
+    filter(doi_suffix %in% undocumented_doi_suffixes) %>% 
+    filter(avg_text != FALSE) %>% 
+    nrow()
+
+est_count_undocumented_with_avg_text_no_avg_method <- 
+    round(count_undocumented_with_avg_text * pct_documented_no_avg_details,0)
+
+est_total_undoc_no_avg_text <- 
+    count_undocumented_no_avg_text + est_count_undocumented_with_avg_text_no_avg_method
+
+est_total_undoc_with_avg_text <- 
+    round(count_undocumented_with_avg_text * (1 - pct_documented_no_avg_details),0)
+
+documented_has_avg_details + documented_no_avg_details + 
+    est_total_undoc_no_avg_text + est_total_undoc_with_avg_text ==
+    total_articles
+
+est_total_with_avg_details <- 
+    documented_has_avg_details + est_total_undoc_with_avg_text
+
+est_total_no_avg_details <- 
+    documented_no_avg_details + est_total_undoc_no_avg_text
+
+avg_documentation_tib <- tibble(
+    has_avg_documentation = c(TRUE, FALSE),
+    estimate = c(est_total_with_avg_details,
+                 est_total_no_avg_details)) %>%
+    mutate(prop = prop.table(estimate),
+           documentation = if_else(has_avg_documentation == TRUE,
+                                   "Describes Averaging Methods",
+                                   "Does NOT Describe Averaging Methods"))
+avg_documentation_tib
+
+ggplot(data = avg_documentation_tib) +
+    geom_col(aes(x = documentation, y = estimate)) +
+    xlab("Averaging Method Documentation") +
+    ylab("Count")
+
+# number of documents w/o any averaging text
+with_avg_text <- merge_df %>% 
+    filter(avg_text != FALSE) %>% 
+    nrow()
+
+avg_text_no_documentation_est <- round(with_avg_text * pct_documented_no_avg_details, 0)
+
+# total - has avg_text + estimate with text but w/o documentation = num undocumented
+nrow(merge_df) - with_avg_text + avg_text_no_documentation_est
+
 
 # count by averaging type
 n_avg <- merge_df %>% 
