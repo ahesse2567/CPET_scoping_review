ce6cd13c080a5e0b46e31b69006b23ca0973d569
Anton Hesse <ahesse2567@gmail.com>
2022-12-16 16:51:33 -0600

    Fixed error of not opening epub and txt
    Also refactored for fewer lines

--- code/cpet_articles/open_full_text.py
+++ code/cpet_articles/open_full_text.py
@@ -1,17 +1,42 @@
 from pathlib import Path
-import re
-# from subprocess 
+import subprocess
 
-full_text_folder_path = Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts')
+def check_quit(test, check = 'q'):
+    if test == check:
+        print("Quitting program\n")
+        quit()
 
-file_name = str(input('Enter the name of the file you wish to open: '))
-file_stem = Path(file_name).stem
+def open_full_text_file():
+    # opens a full-text file in the order of pdf, epub, then txt
+    file_name = str(input('Enter the file name (DOI suffix) of the file you wish to open, or enter "q" to quit: '))
+    check_quit(file_name)
 
-extensions = ['pdfs', 'epubs', 'txts']
+    full_text_folder_path = Path('/Users/antonhesse/Desktop/Anton/Education/UMN/Lab and Research/HSPL/CPET_scoping_review/data/cpet_articles/full_texts')
 
-for ext in extensions:
-    ext_singular = re.sub(r's$', '', ext)
-    file_paths = list(Path(full_text_folder_path / ext).rglob(f'*.{ext_singular}'))
-    stems = [path.stem for path in file_paths]
-    if file_stem in stems:
+    full_text_file_extensions = ['pdf', 'epub', 'txt']
+
+    for file_ext in full_text_file_extensions:
+        file_path = Path(full_text_folder_path / (file_ext + 's') / (file_name + '.' + file_ext))
+        if file_ext == 'pdf':
+            cmd = f'open -a "Adobe Acrobat" "{file_path}"'
+        else:
+            cmd = f'open "{file_path}"'
+
+        run = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
         try:
+            out, err = run.communicate(timeout=5)
+            if err:
+                continue
+            else:
+                open_full_text_file()
+        except Exception as e:
+            run.kill()
+            out, err = run.communicate()
+            if err:
+                continue
+    # If it gets to this point, the file can't be found.
+    print(f'{file_name} not found\n')
+    open_full_text_file()
+
+if __name__ == "__main__":
+    open_full_text_file()
