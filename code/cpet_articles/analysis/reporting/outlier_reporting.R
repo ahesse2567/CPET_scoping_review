04ac32fbed55c7569302b545724d44178ff182c2
Anton Hesse <ahesse2567@gmail.com>
2023-01-12 11:11:21 -0600

    Updated reporting scripts after mtng w/ Chris

--- code/cpet_articles/analysis/reporting/outlier_reporting.R
+++ code/cpet_articles/analysis/reporting/outlier_reporting.R
@@ -3,7 +3,12 @@ library(stringr)
 library(scales)
 library(janitor)
 
-ineligible_articles <- read_csv("data/cpet_articles/text_analysis/combined_ineligible_articles.csv") %>% 
+# re-find which articles are ineligible in case I forgot to update this manaully
+source("code/cpet_articles/tidying/combine_ineligible_articles.R")
+
+ineligible_articles <- read_csv(
+    "data/cpet_articles/text_analysis/ineligible_articles_combined.csv",
+    show_col_types = FALSE) %>% 
     clean_names()
 
 # load_bbb articles, removing potential Douglas bag or mixing chamber articles
@@ -16,27 +21,42 @@ outlier_data <- read_csv("data/cpet_articles/text_analysis/Outliers - Outliers.c
                          show_col_types = FALSE) %>% 
     clean_names()
 
-merge_df <- full_join(bbb_articles, outlier_data, by = "doi_suffix") %>% 
+outlier_df <- full_join(bbb_articles, outlier_data, by = "doi_suffix") %>% 
     select(colnames(outlier_data)) %>% 
     filter(doi_suffix %in% bbb_articles$doi_suffix)
-    
+
 
 ############### OUTLIERS #################
 
-total_articles <- merge_df %>% 
+total_articles <- outlier_df %>% 
     distinct(doi_suffix, .keep_all = FALSE) %>% 
     nrow()
 
-articles_reporting_outliers <- merge_df %>% 
+articles_reporting_outliers_tib <- outlier_df %>% 
     distinct(doi_suffix, .keep_all = TRUE) %>% 
-    mutate(outlier_documentation = is.na(outlier_limit)) %>% 
+    mutate(outlier_documentation = !is.na(outlier_limit)) %>% 
     count(outlier_documentation) %>% 
     ungroup() %>% 
     mutate(pct = prop.table(n))
-articles_reporting_outliers
+articles_reporting_outliers_tib
+
+articles_reporting_outliers <- articles_reporting_outliers_tib %>%
+    filter(outlier_documentation == TRUE) %>% 
+    select(n) %>% pull()
+
+pct_articles_reporting_outliers <- articles_reporting_outliers_tib %>%
+    filter(outlier_documentation == TRUE) %>% 
+    select(pct) %>%
+    pull() %>% 
+    round(3)
+
+z <- qnorm(0.025, lower.tail = FALSE)
+moe_pct_articles_reporting <-
+    z * sqrt((pct_articles_reporting_outliers * (1 - pct_articles_reporting_outliers)) / total_articles)
+
 
 # count by outlier cutoff type
-outlier_cutoff_by_type <- merge_df %>% 
+outlier_cutoff_by_type <- outlier_df %>% 
     group_by(outlier_limit) %>% 
     summarize(n = n()) %>% 
     ungroup() %>% 
@@ -44,7 +64,7 @@ outlier_cutoff_by_type <- merge_df %>%
     arrange(desc(n))
 outlier_cutoff_by_type
 
-outlier_reporting_frequency_plot <- merge_df %>% 
+outlier_reporting_frequency_plot <- outlier_df %>% 
     count(outlier_limit) %>% 
     mutate(pct = round(prop.table(n),4)) %>% 
     mutate(outlier_limit = if_else(
@@ -67,26 +87,28 @@ outlier_reporting_frequency_plot <- merge_df %>%
     #         paste(
     #             "Outlier cutoff reporting frequency. Data are expressed as counts and percentages. N = ",
     #             total_articles, ".", sep = ""), width = 100)) +
-    theme(plot.caption = element_text(hjust=0))
+    theme(plot.caption = element_text(hjust=0),
+          text=element_text(family="Times New Roman", size=12))
 outlier_reporting_frequency_plot
 
 
 # pct outlier cutoff by type
-specified_outlier_cutoffs_by_type <- merge_df %>% 
+specified_outlier_cutoffs_by_type <- outlier_df %>% 
     select(outlier_limit) %>% 
     drop_na() %>% 
     group_by(outlier_limit) %>% 
     summarize(n = n()) %>% 
-    mutate(freq = n / sum(n) * 100)
+    ungroup() %>% 
+    mutate(freq = prop.table(n))
 specified_outlier_cutoffs_by_type
 
 # count of articles that specify outlier removal
-count_outlier_procedure_described <- merge_df %>% 
+count_outlier_procedure_described <- outlier_df %>% 
     select(outlier_limit) %>% 
     summarize_all(list(count_outlier_procedure_described = ~ sum(!is.na(.)))) %>% 
     pull()
 
-pct_outlier_limits_plot <- merge_df %>% 
+pct_outlier_limits_plot <- outlier_df %>% 
     select(outlier_limit) %>% 
     drop_na() %>% 
     group_by(outlier_limit) %>% 
@@ -97,25 +119,127 @@ pct_outlier_limits_plot <- merge_df %>%
     summarize(n = sum(n)) %>% 
     ungroup() %>% 
     mutate(pct = prop.table(n)) %>% 
-    mutate(outlier_limit = str_to_title(outlier_limit)) %>% 
+    mutate(outlier_limit = if_else(outlier_limit == "other",
+                                   str_to_title(outlier_limit),
+                                   outlier_limit)) %>% 
     ggplot(aes(x = outlier_limit, y = n)) +
     geom_col() +
     geom_text(aes(label = scales::percent(pct)), vjust = -0.5) +
     geom_text(aes(label = n), vjust = -2) +
     xlab("Outlier Limit") +
     ylab("Count") +
-    ylim(0, 200 * ceiling(max(specified_outlier_cutoffs_by_type$n) / 200)) +
+    ylim(0, 250) +
     theme_minimal() +
     # labs(
     #     caption = str_wrap(
     #         paste("Outlier cutoff frequencies. Data are expressed as counts and percentages. N = ",
     #               count_outlier_procedure_described, ".", sep = ""), width = 100)) +
-    theme(plot.caption = element_text(hjust=0))
+    theme(plot.caption = element_text(hjust=0),
+          text=element_text(family="Times New Roman", size=12))
 pct_outlier_limits_plot
 
 
 # percentage of articles that specify outlier removal
-merge_df %>% 
+outlier_df %>% 
     select(outlier_limit) %>% 
     summarize_all(list(
         pct_outlier_procedure_described = ~ sum(!is.na(.)) / length(.)*100))
+
+outlier_function_tib <- outlier_df %>% 
+    rowwise() %>% 
+    mutate(outlier_avg_func = any(!is.na(c(outlier_avg_type,
+                                           outlier_avg_subtype,
+                                           outlier_avg_amount,
+                                           outlier_avg_mos,
+                                           outlier_avg_mean_type)))) %>% 
+    count(outlier_avg_func) %>% 
+    ungroup() %>% 
+    mutate(pct = prop.table(n))
+outlier_function_tib
+
+n_outlier_func_reporting <- outlier_function_tib %>% 
+    filter(outlier_avg_func == TRUE) %>% 
+    select(n) %>% 
+    pull()
+
+pct_outlier_func_reporting <- outlier_function_tib %>% 
+    filter(outlier_avg_func == TRUE) %>% 
+    select(pct) %>% 
+    pull()
+
+moe_pct_outlier_func_reporting <-
+    z * sqrt((pct_outlier_func_reporting * (1 - pct_outlier_func_reporting)) / total_articles)
+
+
+outlier_func_by_type <- outlier_df %>% 
+    rowwise() %>% 
+    mutate(outlier_avg_func = any(!is.na(c(outlier_avg_type,
+                                           outlier_avg_subtype,
+                                           outlier_avg_amount,
+                                           outlier_avg_mos,
+                                           outlier_avg_mean_type)))) %>% 
+    filter(outlier_avg_func== TRUE) %>% 
+    count(outlier_avg_type) %>% 
+    ungroup() %>% 
+    mutate(pct = prop.table(n)) %>% 
+    arrange(desc(n))
+outlier_func_by_type
+
+###### By type
+
+n_breath_outlier_funcs <- outlier_func_by_type %>% 
+    filter(outlier_avg_type == "breath") %>% 
+    select(n) %>% 
+    pull()
+
+pct_breath_outlier_funcs <- outlier_func_by_type %>% 
+    filter(outlier_avg_type == "breath") %>% 
+    select(pct) %>% 
+    pull()
+pct_breath_outlier_funcs <- sprintf("%.1f", pct_breath_outlier_funcs * 100)
+
+n_time_outlier_funcs <- outlier_func_by_type %>% 
+    filter(outlier_avg_type == "time") %>% 
+    select(n) %>% 
+    pull()
+
+pct_time_outlier_funcs <- outlier_func_by_type %>% 
+    filter(outlier_avg_type == "time") %>% 
+    select(pct) %>% 
+    pull()
+pct_time_outlier_funcs <- sprintf("%.1f", pct_time_outlier_funcs * 100)
+
+outlier_func_by_type_amount <- outlier_df %>% 
+    rowwise() %>% 
+    mutate(outlier_avg_func = any(!is.na(c(outlier_avg_type,
+                                           outlier_avg_subtype,
+                                           outlier_avg_amount,
+                                           outlier_avg_mos,
+                                           outlier_avg_mean_type)))) %>% 
+    filter(outlier_avg_func== TRUE) %>% 
+    count(outlier_avg_type, outlier_avg_amount) %>% 
+    ungroup() %>% 
+    mutate(pct = prop.table(n)) %>% 
+    arrange(desc(n))
+
+# organize by type and THEN by frequency
+# some kind of double line between breath and time methods so readers
+# can differentiate those faster
+
+outlier_func_by_type_amount
+
+#$ calculate number and pct that did both limit and function but no table
+
+
+###### 5 breath functions
+
+n_5_breath_func <- outlier_func_by_type_amount %>% 
+    filter(outlier_avg_amount == 5 & outlier_avg_type == "breath") %>% 
+    select(n) %>% 
+    pull()
+
+pct_5_breath_func <- outlier_func_by_type_amount %>% 
+    filter(outlier_avg_amount == 5 & outlier_avg_type == "breath") %>% 
+    select(pct) %>% 
+    pull()
+pct_5_breath_func <- sprintf("%.1f", pct_5_breath_func * 100)
